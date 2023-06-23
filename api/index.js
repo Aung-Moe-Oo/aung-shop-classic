5fa31cb31a38f3a0f245c613e69467e4a933e454
saiaungmoeoo <saiaungmoeoo.lk@gmail.com>
2023-06-23 10:24:11 +0630

    fix bugs

--- api/index.js
+++ api/index.js
@@ -38,16 +38,16 @@ app.use("/api/mail", require(path.join(__dirname, "/routes/mail.js")));
 
 // static files (build of your frontend)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
 // app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+//   res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
 // });
 
 // app.listen(process.env.PORT || 5000, () => {
