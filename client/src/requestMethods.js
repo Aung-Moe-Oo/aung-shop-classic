f9adebd79d4aefa486bbe754c07541e30cefaae6
saiaungmoeoo <saiaungmoeoo.lk@gmail.com>
2023-06-23 14:28:57 +0630

    add searched items

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
-// const BASE_URL = "https://aung-shop-classic.vercel.app/api/";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://aung-shop-classic.vercel.app/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
